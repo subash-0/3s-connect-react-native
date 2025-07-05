827f17eb70d2e80541c1ff2bb35f7d0739b93903
subash <subashwp2@gmail.com>
2025-07-06 00:55:58 +0545

    added tialwind css

---
+++ mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import {Ionicons} from "@expo/vector-icons"
+
+export default function Index() {
+  return (
+    <View
+     className="flex-1 justify-center items-center bg-white"
+    >
+      <Text className="text-xl font-bold text-blue-500">Welcom to expo</Text>
+    </View>
+  );
+}
